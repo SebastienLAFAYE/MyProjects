fbb4982e9ff177f8bb7950afa06d4796f85b5799
LAFAYE Sébastien <lafaye_s@etna-alternance.net>
2020-04-29 19:22:48 +0200

    paypal

--- serveur/config/paypalConfig.js
+++
@@ -1,7 +0,0 @@
-const paypalConfig = {
-    'mode': 'sandbox',
-    'client_id': 'Af536eiuRbYQZm-x7PuwHsH8Esrz6aYIBCiyotxcbPXXU22j2ccvFiGBqCbhzRlbqBycNZLQTDYw_9Vb',
-    'client_secret': 'EB_TcoLOg2SCrZ19RBguUSPenouNcndMcTtUx-qBd-_WEKHraRrrKz13TqGgj4jNwZ_tDsgqOE-cu0Ox'
-}
-
-export default paypalConfig;
--- serveur/config/prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    discordClientID: process.env.DISCORD_CLIENT_ID,
-    discordClientSecret: process.env.DISCORD_CLIENT_SECRET,
-    scopes: ['identify', 'email', 'guilds', 'guilds.join']
-};
