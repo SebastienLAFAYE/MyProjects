2213956f24f7707a2db035b27dfb1d7bebec6f4b
LAFAYE Sébastien <lafaye_s@etna-alternance.net>
2020-04-29 19:25:43 +0200

    paypal

--- serveur/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    discordClientID: '704598772419788910',
-    discordClientSecret: 'ADeI2Dpr2ZfmgvxlQFNTmIMS642ZPvCG',
-    scopes: ['identify', 'email', 'guilds', 'guilds.join']
-};
